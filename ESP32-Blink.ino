a8f3ecc1d718d0e7b0b37baea7cf042bddc6310a
dongocthanhchau <dongocthanhchau@gmail.com>
2021-11-28 10:58:25 +0700

    fix define1

--- ESP32-Blink.ino
+++ ESP32-Blink.ino
@@ -1,14 +1,14 @@
-#define LEDPIN 14
+#define PINLED 14
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LEDPIN, OUTPUT);
+  pinMode(PINLED, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LEDPIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(PINLED, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(LEDPIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(PINLED, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
